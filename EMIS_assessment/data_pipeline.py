3c0723f806fe356ea1fa5d045adcf1cbb64c6e7d
Ananth Ravichandran <ananth2310@gmail.com>
2024-05-12 00:46:16 +0530

    Provenance func implnt

--- EMIS_assessment/data_pipeline.py
+++ EMIS_assessment/data_pipeline.py
@@ -12,27 +12,140 @@ def ExplanationOfBenefit():
     pass
 def SupplyDelivery():
     pass
-def DocumentReference():
-    pass
-def Provenance():
-    pass
+def DocumentReference(data):
+    print(data)
+def Provenance(data):
+    print(data['id'])
+    for target in data['target']:
+        print(target['reference'])
+    print(data['recorded'])
+    for agent in data['agent']:
+        print(agent['type']['text'])
+        print(agent['who']['display'])
+        print(agent['onBehalfOf']['display'])
 
-def CareTeam():
-    pass
+def CareTeam(data):
+    print(data['resource']['id'])
+    print(data['resource']['status'])
+    print(data['resource']['subject']['reference'])
+    print(data['resource']['encounter']['reference'])
+    print(data['resource']['period']['start'])
+    if 'end' in data['resource']:
+        print(data['resource']['period']['end'])
+    for participant in data['resource']['participant']:
+        print(participant['role'][0]['coding'][0]['code'])
+        print(participant['role'][0]['coding'][0]['display'])
+        print(participant['member']['display'])
+    if 'reasonCode' in  data['resource']:
+        for reasoncode in data['resource']['reasonCode']:
+            print(reasoncode['coding'][0]['code'])
+            print(reasoncode['coding'][0]['display'])
+    print(data['resource']['managingOrganization'][0]['display'])
 
-def ImagingStudy():
-    pass
 
-def AllergyIntolerance():
-    pass
-def Encounter():
-    pass
+def ImagingStudy(data):
+    print(data['resource']['id'])
+    print(data['resource']['identifier'][0]['system'])
+    print(data['resource']['identifier'][0]['value'])
+    print(data['resource']['status'])
+    print(data['resource']['subject']['reference'])
+    print(data['resource']['encounter']['reference'])
+    print(data['resource']['started'])
+    print(data['resource']['numberOfSeries'])
+    print(data['resource']['numberOfInstances'])
+    print(data['resource']['procedureCode'][0]['coding'][0]['code'])
+    print(data['resource']['procedureCode'][0]['coding'][0]['display'])
+    print(data['resource']['location']['display'])
+    print(data['resource']['series'][0]['uid'])
+    for series in data['resource']['series']:
+        print(series['uid'])
+        print(series['number'])
+        print(series['modality']['code'])
+        print(series['modality']['display'])
+        print(series['numberOfInstances'])
+        print(series['bodySite']['code'])
+        print(series['bodySite']['display'])
+        print(series['started'])
+        for instance in series['instance']:
+            print(instance['uid'])
+            print(instance['number'])
+            print(instance['title'])
 
-def Condition():
-    pass
 
-def MedicationRequest():
-    pass
+def AllergyIntolerance(data):
+    print(data['resource']['id'])
+    print(data['resource']['clinicalStatus']['coding'][0]['code'])
+    print(data['resource']['verificationStatus']['coding'][0]['code'])
+    print(data['resource']['type'])
+    print(data['resource']['category'][0])
+    print(data['resource']['criticality'])
+    print(data['resource']['code']['coding'][0]['code'])
+    print(data['resource']['code']['coding'][0]['display'])
+    print(data['resource']['patient']['reference'])
+    print(data['resource']['recordedDate'])
+
+def Encounter(data):
+    print(data['resource']['id'])
+    print(data['resource']['identifier'][0]['value'])
+    print(data['resource']['status'])
+    print(data['resource']['class']['code'])
+    print(data['resource']['type'][0]['coding'][0]['code'])
+    print(data['resource']['type'][0]['coding'][0]['display'])
+    print(data['resource']['subject']['reference'])
+    print(data['resource']['subject']['display'])
+    print(data['resource']['participant'][0]['type'][0]['coding'][0]['code'])
+    print(data['resource']['participant'][0]['type'][0]['coding'][0]['display'])
+    print(data['resource']['participant'][0]['period']['start'])
+    print(data['resource']['participant'][0]['period']['end'])
+    print(data['resource']['participant'][0]['individual']['display'])
+    print(data['resource']['participant'][0]['period']['start'])
+    print(data['resource']['participant'][0]['period']['end'])
+    print(data['resource']['location'][0]['location']['display'])
+    print(data['resource']['serviceProvider']['display'])
+
+def Condition(data):
+    print(data['resource']['id'])
+    print(data['resource']['clinicalStatus']['coding'][0]['code'])
+    print(data['resource']['verificationStatus']['coding'][0]['code'])
+    print(data['resource']['category'][0]['coding'][0]['code'])
+    print(data['resource']['code']['coding'][0]['code'])
+    print(data['resource']['code']['coding'][0]['display'])
+    print(data['resource']['subject']['reference'])
+    print(data['resource']['encounter']['reference'])
+    print(data['resource']['onsetDateTime'])
+    if 'abatementDateTime' in data['resource']:
+        print(print(data['resource']['abatementDateTime']))
+    print(data['resource']['recordedDate'])
+
+def MedicationRequest(data):
+    print(data['resource']['id'])
+    print(data['resource']['status'])
+    print(data['resource']['intent'])
+    if 'medicationCodeableConcept' in data['resource']:
+        for medicationcodeableconcept in data['resource']['medicationCodeableConcept']['coding']:
+            print(medicationcodeableconcept['code'])
+            print(medicationcodeableconcept['display'])
+    print(data['resource']['subject']['reference'])
+    print(data['resource']['encounter']['reference'])
+    print(data['resource']['authoredOn'])
+    print(data['resource']['requester']['display'])
+    if 'reasonReference' in data['resource']:
+        print(data['resource']['reasonReference'][0]['reference'])
+    if 'dosageInstruction' in data['resource']:
+        for dosageinstruction in data['resource']['dosageInstruction']:
+            print(dosageinstruction['sequence'])
+            if 'timing' in dosageinstruction:
+                print(dosageinstruction['timing']['repeat']['frequency'])
+                print(dosageinstruction['timing']['repeat']['period'])
+                print(dosageinstruction['timing']['repeat']['periodUnit'])
+            else:
+                print(dosageinstruction['text'])
+            if dosageinstruction['asNeededBoolean'] == False:
+                for doseandrate in dosageinstruction['doseAndRate']:
+                    print(doseandrate['type']['coding'][0]['code'])
+                    print(doseandrate['type']['coding'][0]['display'])
+                    print(doseandrate['doseQuantity']['value'])
+
 def Claim(data):
     print(data['resource']['id'])
     print(data['resource']['status'])
@@ -190,9 +303,33 @@ def read_json_data(files,dir_location):
                 pass
                 #Observation(data)
             elif data['resource']['resourceType'] == 'Claim':
-                Claim(data)
+                pass
+                #Claim(data)
+            elif data['resource']['resourceType'] == 'MedicationRequest':
+                pass
+                #MedicationRequest(data)
+            elif data['resource']['resourceType'] == 'Condition':
+                pass
+                #Condition(data)
+            elif data['resource']['resourceType'] == 'Encounter':
+                pass
+                #Encounter(data)
+            elif data['resource']['resourceType'] == 'AllergyIntolerance':
+                pass
+                #AllergyIntolerance(data)
+            elif data['resource']['resourceType'] == 'ImagingStudy':
+                pass
+                #ImagingStudy(data)
+            elif data['resource']['resourceType'] == 'CareTeam':
+                pass
+                #CareTeam(data)
+            elif data['resource']['resourceType'] == 'Provenance':
+                #Provenance(data['resource'])
+                pass
+            elif data['resource']['resourceType'] == 'DocumentReference':
+                DocumentReference(data['resource'])
 
-               
+
 
         f.close()
     
